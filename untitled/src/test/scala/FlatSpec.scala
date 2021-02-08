9cb0e4f8d3e86931186b02b8eaabedc5707bf3d8
QAcpardeep <cpardeep@qa.com>
2021-02-08 11:48:04 +0000

    gg

--- untitled/src/test/scala/FlatSpec.scala
+++ untitled/src/test/scala/FlatSpec.scala
@@ -1,4 +1,16 @@
-class FlatSpec extends FlatSpecBaseClass {
+import org.scalatest.flatspec.AnyFlatSpec
+class FlatSpec extends AnyFlatSpec {
+
+  behavior of "the tv"
+
+  "when calling the isOn()" should "return true" in{
+    val tv = new TV(true)
+
+    val value = tv.isOn()
+
+    assert(value == true)
+
+  }
 
 
 }
--- untitled/src/test/scala/FlatSpecBaseClass.scala
+++
@@ -1,5 +0,0 @@
-import org.scalatest.Matchers
-
-abstract class FlatSpecBaseClass extends FlatSpecBaseClass with Matchers{
-
-}
